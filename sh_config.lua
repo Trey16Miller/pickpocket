38f72da6a1d7abc7e24e4ece673a5d5431bf2e6c
Craigy131 <65000316+Trey16Miller@users.noreply.github.com>
2026-01-19 03:11:11 -0500

    Create sh_config.lua

---
+++ sh_config.lua
@@ -0,0 +1,20 @@
+PICKPOCKET = PICKPOCKET or {}
+
+PICKPOCKET.Range = 80
+PICKPOCKET.RequireCrouch = true
+
+PICKPOCKET.AwarenessMax = 100
+PICKPOCKET.AwarenessGainFront = 35
+PICKPOCKET.AwarenessGainSide = 12
+PICKPOCKET.AwarenessGainBehindSneak = 1
+PICKPOCKET.AwarenessDecay = 20
+
+PICKPOCKET.TickRate = 0.1
+
+PICKPOCKET.MoneyMin = 10
+PICKPOCKET.MoneyMax = 120
+
+PICKPOCKET.FailCooldown = 3
+PICKPOCKET.SuccessCooldown = 1.2
+
+PICKPOCKET.LoseSightTime = 1.5
